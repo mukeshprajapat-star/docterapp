e11752c6d56ac921fb4c3048674487b19ea2cefb
mukesh prajapat <mukeshprajapat7529@gmail.com>
2022-11-04 09:00:27 +0530

    initial commit

--- backend/server.js
+++ backend/server.js
@@ -32,10 +32,10 @@ app.use('/api/authUser', authsUser);
 app.use('/api/profile', profile);
 app.use('/api/appointment', appointment);
 
-app.use(express.static(path.join(__dirname,"../frontend/build")));
+app.use(express.static(path.join(__dirname,"../client/build")));
 
 app.get("*",(req,res)=>{
-    res.sendFile(path.resolve(__dirname,"../frontend/build/index.html"));
+    res.sendFile(path.resolve(__dirname,"../client/build/index.html"));
 });
 
 
