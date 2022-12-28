ea38b71c095e32feb2f0138c6754f9d565a3823d
mukesh prajapat <mukeshprajapat7529@gmail.com>
2022-12-28 09:28:11 +0530

    fixed bugs

--- backend/server.js
+++ backend/server.js
@@ -31,9 +31,9 @@ app.use('/api/profile', profile);
 app.use('/api/appointment', appointment);
 
 if (process.env.NODE_ENV === "production") {
-    app.use(express.static(path.join(__dirname,"client","build")));
+    app.use(express.static(path.join(__dirname,"../client/build")));
     app.get("*", (req, res) =>
-      res.sendFile(path.resolve(__dirname, "client","build","index.html"))
+      res.sendFile(path.resolve(__dirname, "../client/build/index.html"))
     );
   } else {
     app.get("/", (req, res) => {
